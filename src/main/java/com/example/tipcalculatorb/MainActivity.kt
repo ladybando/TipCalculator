c6d596ba50c011ad9a62664efb42a3c0591f1026
Ayana Bando <ayanaflax@yahoo.com>
2021-08-02 20:27:38 -0400

    update main activity for scrolling and enter on soft kybord

--- src/main/java/com/example/tipcalculatorb/MainActivity.kt
+++ src/main/java/com/example/tipcalculatorb/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.tipcalculatorb
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.example.tipcalculatorb.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
@@ -12,7 +16,7 @@ class   MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode) }
         binding.calculateButton.setOnClickListener{ calculateTip() }
     }
 
@@ -41,5 +45,16 @@ class   MainActivity : AppCompatActivity() {
     private fun displayTip(tip: Double){
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text =  getString(R.string.tip_amount, formattedTip)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
